645991b783abd564d89c692587ae5478b236ac47
Soham Sharma <42056502+soham2710@users.noreply.github.com>
2024-09-15 07:46:37 +0530

    Update app.py
    Added Render Port

--- app.py
+++ app.py
@@ -696,4 +696,6 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  # Default to port 8000 if PORT is not set
+    app.run(host="0.0.0.0", port=port, debug=True)
